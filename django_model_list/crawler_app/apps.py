6541a2de5e7decfb6ccb3d4acbc73d35e26fa2af
hyeseongJeong <tjdehfldk@gmail.com>
2019-01-07 09:18:06 +0900

    good

--- django_model_list/crawler_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrawlerAppConfig(AppConfig):
-    name = 'crawler_app'
--- job_parsing/job_parsing/category/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ListParsingConfig(AppConfig):
-    name = 'category'
